f3f00792bf9fd88be7ea302727ab784401603212
rverani <37932716+rverani@users.noreply.github.com>
2020-10-22 03:57:39 -0300

    Add files via upload

---
+++ precache-manifest.8c578a25e366e84018e506d03f689fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512c8f9e07c913620f7b01dc68fceca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "054e8526d21acb746ccc",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "054e8526d21acb746ccc",
+    "url": "/static/js/2.bd230520.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.bd230520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d45a9920873c30a1471",
+    "url": "/static/js/main.a3e03f91.chunk.js"
+  },
+  {
+    "revision": "269bf64c0faf6a9b56c1",
+    "url": "/static/js/runtime-main.f4a2355d.js"
+  }
+]);
